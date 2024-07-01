3f6e065e12421f02f9e35016d6b145c0a5a1fd2e
Gustavo Raimo <gustavo.raimo92@gmail.com>
2024-07-01 20:51:43 -0300

    Finalização do projeto

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,40 @@
+import Tag from '../Tag'
+import Button from '../Button'
+import Loader from '../Loader'
+
+import { useGetFeatureGameQuery } from '../../services/api'
+import { parceToBrl } from '../../utils'
+
+import * as S from './styles'
+
+const Banner = () => {
+  const { data: game } = useGetFeatureGameQuery()
+
+  if (!game) {
+    return <Loader />
+  }
+  return (
+    <S.Image style={{ backgroundImage: `url(${game?.media.cover})` }}>
+      <div className="container">
+        <Tag size="big">Destaque do dia</Tag>
+        <div>
+          <S.Title>{game.name} </S.Title>
+          <S.Prices>
+            De <span>{parceToBrl(game.prices.old)}</span> <br />
+            por apenas {parceToBrl(game.prices.current)}
+          </S.Prices>
+        </div>
+        <Button
+          type="link"
+          to={`/product/${game.id}`}
+          title="Clique aqui para aproveitar essa oferta"
+          variant="primary"
+        >
+          Aproveitar
+        </Button>
+      </div>
+    </S.Image>
+  )
+}
+
+export default Banner
